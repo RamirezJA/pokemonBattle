7b65817487f5d7ebace736297532e0868af256fe
Jose Ramirez <26813102+RamirezJA@users.noreply.github.com>
2023-04-23 00:59:20 -0700

    battle logic set

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -2,10 +2,39 @@ import styles from "../styles/battle.module.css"
 import fire from "../../public/fire.png"
 import grass from "../../public/grass.png"
 import water from "../../public/water-drop.png"
-
 import Image from "next/image"
+import { useEffect, useState } from "react"
 
 export default function Battle() {
+  const [firstChoice, setFirstChoice] = useState("")
+  const [secondChoice, setSecondChoice] = useState("")
+
+  const handleFirstChoice = (choice) => {
+    setFirstChoice(choice)
+    console.log(choice)
+  }
+
+  const handleSecondChoice = (choice) => {
+    setSecondChoice(choice)
+    console.log(choice)
+  }
+
+  const compareChoices = () => {
+    const winners = {
+      fire: "grass",
+      water: "fire",
+      grass: "water",
+    }
+
+    if (firstChoice === secondChoice) {
+      console.log("Tie game!")
+    } else if (winners[firstChoice] === secondChoice) {
+      console.log(`${firstChoice} beats ${secondChoice}!`)
+    } else {
+      console.log(`${secondChoice} beats ${firstChoice}!`)
+    }
+  }
+
   return (
     <>
       <div className={styles.container}>
@@ -13,29 +42,23 @@ export default function Battle() {
           <div className={styles.name}>Name</div>
           <div className={styles.player}>Player</div>
           <div className={styles.options}>
-            <div className={styles.fire}>
-              <Image
-                src={fire}
-                alt='Picture of the author'
-                width={50}
-                height={50}
-              />
+            <div
+              className={styles.fire}
+              onClick={() => handleFirstChoice("fire")}
+            >
+              <Image src={fire} alt='fire icon' width={50} height={50} />
             </div>
-            <div className={styles.water}>
-              <Image
-                src={water}
-                alt='Picture of the author'
-                width={50}
-                height={50}
-              />
+            <div
+              className={styles.water}
+              onClick={() => handleFirstChoice("water")}
+            >
+              <Image src={water} alt='water icon' width={50} height={50} />
             </div>
-            <div className={styles.grass}>
-              <Image
-                src={grass}
-                alt='Picture of the author'
-                width={50}
-                height={50}
-              />
+            <div
+              className={styles.grass}
+              onClick={() => handleFirstChoice("grass")}
+            >
+              <Image src={grass} alt='grass icon' width={50} height={50} />
             </div>
           </div>
         </div>
@@ -44,31 +67,28 @@ export default function Battle() {
           <div className={styles.name}>Name</div>
           <div className={styles.player}>Player</div>
           <div className={styles.options}>
-            <div className={styles.fire}>
-              <Image
-                src={fire}
-                alt='Picture of the author'
-                width={50}
-                height={50}
-              />
+            <div
+              className={styles.fire}
+              onClick={() => handleSecondChoice("fire")}
+            >
+              <Image src={fire} alt='fire icon' width={50} height={50} />
             </div>
-            <div className={styles.water}>
-              <Image
-                src={water}
-                alt='Picture of the author'
-                width={50}
-                height={50}
-              />
+            <div
+              className={styles.water}
+              onClick={() => handleSecondChoice("water")}
+            >
+              <Image src={water} alt='water icon' width={50} height={50} />
             </div>
-            <div className={styles.grass}>
-              <Image
-                src={grass}
-                alt='Picture of the author'
-                width={50}
-                height={50}
-              />
+            <div
+              className={styles.grass}
+              onClick={() => handleSecondChoice("grass")}
+            >
+              <Image src={grass} alt='grass icon' width={50} height={50} />
             </div>
           </div>
+        </div>
+        <div className={styles.vs} onClick={() => compareChoices()}>
+          Battle
         </div>
       </div>
     </>
